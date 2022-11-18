cf5cb1125517342b19329c67ddb3f31f3e94e59d
singharaj <djjd22938@gmail.com>
2022-11-18 15:10:42 -0500

    better routes format

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const animalRoutes = require('././animalRoutes');
-const zookeeperRoutes = require('././zookeeperRoutes');
+const animalRoutes = require('../apiRoutes/animalRoutes');
+const zookeeperRoutes = require('../apiRoutes/zookeeperRoutes');
 
 router.use(animalRoutes);
 router.use(zookeeperRoutes);
