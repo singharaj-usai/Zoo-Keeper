d1eceacb2a38d8a202cd7edcc99fe04603f17f54
singharaj <djjd22938@gmail.com>
2022-11-18 15:37:22 -0500

    tests files

---
+++ __tests__/animals.test.js
@@ -0,0 +1,90 @@
+const fs = require('fs');
+const {
+    filterByQuery,
+    findById,
+    createNewAnimal,
+    validateAnimal
+} = require('../lib/animals');
+const { animals } = require('../data/animals');
+
+jest.mock('fs');
+
+test("creates an animal object", () => {
+    const animal = createNewAnimal(
+        { name: "Darlene", id: "1408" },
+        animals
+    );
+
+    expect(animal.name).toBe("Darlene");
+    expect(animal.id).toBe("1408");
+});
+
+test("filters by query", () => {
+    const startingAnimals = [
+        {
+            id: "1",
+            name: "Travis",
+            species: "chimpanzee",
+            diet: "herbivore",
+            personalityTraits: ["anxious", "goofy"],
+        },
+        {
+            id: "2",
+            name: "Koko",
+            species: "gorilla",
+            diet: "herbivore",
+            personalityTraits: ["quirky", "rash"],
+        },
+    ];
+
+    const updatedAnimals = filterByQuery({ species: "gorilla" }, startingAnimals);
+
+    expect(updatedAnimals.length).toEqual(1);
+});
+
+test("finds by id", () => {
+    const startingAnimals = [
+        {
+            id: "1",
+            name: "Travis",
+            species: "chimpanzee",
+            diet: "herbivore",
+            personalityTraits: ["anxious", "goofy"],
+        },
+        {
+            id: "2",
+            name: "Koko",
+            species: "gorilla",
+            diet: "herbivore",
+            personalityTraits: ["quirky", "rash"],
+        },
+    ];
+
+    const result = findById("3", startingAnimals);
+
+    expect(result.name).toBe("Erica");
+});
+
+test("validates personality traits", () => {
+    const animal = {
+        id: "1",
+        name: "Travis",
+        species: "chimpanzee",
+        diet: "herbivore",
+        personalityTraits: ["anxious", "goofy"],
+    };
+
+    const invalidAnimal = {
+        id: "2",
+        name: "Koko",
+        species: "gorilla",
+        diet: "herbivore",
+        personalityTraits: ["quirky", "rash"],
+    }
+
+    const result = validateAnimal(animal);
+    const result2 = validateAnimal(invalidAnimal);
+
+    expect(result).toBe(true);
+    expect(result2).toBe(false);
+});
