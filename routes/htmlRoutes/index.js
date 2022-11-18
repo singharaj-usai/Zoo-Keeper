a9a665ccd53b3ef1dda657fdea3e6ba70cb9eeca
singharaj <djjd22938@gmail.com>
2022-11-18 15:07:43 -0500

    routers

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,18 +1,22 @@
 const path = require('path');
 const router = require('express').Router();
 
+router.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
 router.get("/", (req, res) => {
     // Sends index.html to be the / page for the site
     res.sendFile(path.join(__dirname, "../../public/index.html"));
 });
 
 router.get("/animals", (req, res) => {
-    // sends animals.html to be the /animals page for the site
+    // Sends animals.html to be the /animals page for the site
     res.sendFile(path.join(__dirname, "../../public/animals.html"));
 });
 
 router.get("/zookeepers", (req, res) => {
-    // sends zookeepers.html to be the /zookeepers page for the site
+    // Sends zookeepers.html to be the /zookeepers page for the site
     res.sendFile(path.join(__dirname, "../../public/zookeepers.html"));
 });
 
