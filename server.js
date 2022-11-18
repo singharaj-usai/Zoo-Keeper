788cc27e6f1ad3765b3aa9974c8c14633d7f393f
singharaj <djjd22938@gmail.com>
2022-11-18 15:21:52 -0500

    express server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+// Serves static files
+app.use(express.static('public'));
+// Parse incoming string or array data
+app.use(express.urlencoded({ extend: true }));
+// Parse incoming JSON data
+app.use(express.json());
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(
+        `API server now on port ${PORT} at http://localhost:${PORT}/api/animals`
+    );
+});
